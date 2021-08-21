3a7d510e120c81155ae7aa36a4211d65adc9c51d
Jacob Stannix <jakestannix at gmail.com>
2021-08-21 06:09:47 -0600

    desktop noweb

--- config/.config/emacs/desktop.el
+++ config/.config/emacs/desktop.el
@@ -33,79 +33,59 @@
   (add-hook 'exwm-update-class-hook #'my/exwm-update-class)
 
   (desktop-environment-mode)
-
-;; These keys should always pass through to Emacs
-(setq exwm-input-prefix-keys
-      '(?\C-x
-        ?\C-u
-        ?\C-h
-        ?\C-w
-        ?\M-x
-        ?\M-`
-        ?\M-&
-        ?\M-:
-        ?\C-\ ))  ;; Ctrl+Space
-
-;;    Ctrl+ Q will enable the next key to
-;;    be sent directly
-(define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
-
-;; Set up global key bindings.  These always work, no matter the input state!
-;; Keep in mind that changing this list after EXWM initializes has no effect.
-(setq exwm-input-global-keys
-      `(
-        ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
-        ([?\s-r] . exwm-reset)
-
-        ;; Move between windows
-        ([?\s-h] . windmove-left)
-        ([?\s-l] . windmove-right)
-        ([?\s-k] . windmove-up)
-        ([?\s-j] . windmove-down)
-
-        ;; Launch applications via shell command
-        ([?\s-&] . (lambda (command)
-                     (interactive (list (read-shell-command "$ ")))
-                     (start-process-shell-command command nil command)))
-
-        ;; Switch workspace
-        ([?\s-w] . exwm-workspace-switch)
-
-        ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
-        ,@(mapcar (lambda (i)
-                    `(,(kbd (format "s-%d" i)) .
-                      (lambda ()
-                        (interactive)
-                        (exwm-workspace-switch-create ,i))))
-                  (number-sequence 0 9))))
-
-(exwm-input-set-key (kbd "s-SPC") 'counsel-linux-app) ;; Set XDG_PATH variables
-
-(defun exwm-poweroff ()
-  (interactive)
-  (when (yes-or-no-p "Power down system? ")
-    (start-process-shell-command "systemctl" nil "systemctl poweroff")))
-(exwm-input-set-key (kbd "C-s-p") 'exwm-poweroff)
-
-(defun exwm-suspend ()
-  (interactive)
-  (when (y-or-n-p "Suspend systeM? ")
-    (start-process-shell-command "systemctl" nil "systemctl suspend")))
-(exwm-input-set-key (kbd "s-p") 'exwm-suspend)
-
-(defun exwm-sys-restart () "Restarts the operating system"
-       (interactive)
-       (when (yes-or-no-p "Restart system? ")
-         (start-process-shell-command "systemctl" nil "systemctl reboot")))
-(exwm-input-set-key (kbd "M-s-p") 'exwm-sys-restart)
-(exwm-input-set-key (kbd "s-q") 'exwm-restart)
-(defun exwm-change-wallpaper () "Changes the Wallpaper"
-  (interactive)
-  (start-process-shell-command "Wallpaper" nil "~/.scripts/wallpaper.sh set"))
-(exwm-input-set-key (kbd "s-y") 'exwm-change-wallpaper)
-
-(exwm-enable))
-
+  ;; These keys should always pass through to Emacs
+  (setq exwm-input-prefix-keys
+        '(?\C-x
+          ?\C-u
+          ?\C-h
+          ?\C-w
+          ?\M-x
+          ?\M-`
+          ?\M-&
+          ?\M-:
+          ?\C-\ ))  ;; Ctrl+Space
+  
+  ;;    Ctrl+ Q will enable the next key to
+  ;;    be sent directly
+  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+  
+  ;; Set up global key bindings.  These always work, no matter the input state!
+  ;; Keep in mind that changing this list after EXWM initializes has no effect.
+  (setq exwm-input-global-keys
+        `(
+          ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
+          ([?\s-r] . exwm-reset)
+  
+          ;; Move between windows
+          ([?\s-h] . windmove-left)
+          ([?\s-l] . windmove-right)
+          ([?\s-k] . windmove-up)
+          ([?\s-j] . windmove-down)
+  
+          ;; Launch applications via shell command
+          ([?\s-&] . (lambda (command)
+                       (interactive (list (read-shell-command "$ ")))
+                       (start-process-shell-command command nil command)))
+  
+          ;; Switch workspace
+          ([?\s-w] . exwm-workspace-switch)
+  
+          ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))))
+  
+  (exwm-input-set-key (kbd "s-SPC") 'counsel-linux-app) ;; Set XDG_PATH variables
+  
+  (defun exwm-change-wallpaper () "Changes the Wallpaper"
+    (interactive)
+    (start-process-shell-command "Wallpaper" nil "~/.scripts/wallpaper.sh set"))
+  (exwm-input-set-key (kbd "s-y") 'exwm-change-wallpaper)
+  
+  (exwm-enable))
 ;; Show battery status in the mode line
 (display-battery-mode 1)
 
