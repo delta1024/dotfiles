bd0a8e258344f2b9528d2651b5cab09e291cb8ab
Jacob Stannix <jakestannix at gmail.com>
2021-08-28 10:10:06 -0600

    locked channels

--- config/.config/guix/channels.scm
+++ config/.config/guix/channels.scm
@@ -2,26 +2,32 @@
 ;;        in Emacs and channels.scm will be generated automatially
 
 (list (channel
-       (name 'flat)
-       (url "https://github.com/flatwhatson/guix-channel.git")
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (commit
+        "0f853ab7731da4784a24cad7b08d6b9b633f5787")
        (introduction
         (make-channel-introduction
-         "33f86a4b48205c0dc19d7c036c85393f0766f806"
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
-          "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
        (name 'nonguix)
        (url "https://gitlab.com/nonguix/nonguix")
+       (commit
+        "46fb69251c49853f72880e4e8a188884525a0379")
        (introduction
         (make-channel-introduction
          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
          (openpgp-fingerprint
           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
-       (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
+       (name 'flat)
+       (url "https://github.com/flatwhatson/guix-channel.git")
+       (commit
+        "7e9eb8464f409bc0d9b821ae4caf44c49f23802a")
        (introduction
         (make-channel-introduction
-         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         "33f86a4b48205c0dc19d7c036c85393f0766f806"
          (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+          "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490")))))
